e6af1a0900e7151923730f44472f533fc6fa78af
dennis <dennis.olimpio0794@gmail.com>
2020-05-20 17:00:03 +0200

    Consegna esercizio v. 1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// input
+
+// età del passeggero
+var etaUtente = parseInt (prompt ('Quanti anni hai?') );
+console.log (etaUtente + ' anni');
+
+// - km viaggio
+var kmDistance = parseInt (prompt ('Quanti km percorrerai?') );
+console.log (kmDistance + ' km');
+
+// - prezzo per km
+var priceKm = 0.21;
+
+// - full price ticket
+var fullTicketPrice = kmDistance * priceKm;
+console.log ('prezzo pieno ' + fullTicketPrice + ' euro');
+
+//- sconto 20% minorenni
+var coupon20 = ((fullTicketPrice * 20) / 100);
+console.log ('sconto under 18: ' + coupon20 + ' euro');
+
+//- prezzo finale per giovani
+var ticketPrice20 = fullTicketPrice - coupon20;
+console.log ('prezzo under 18: ' + ticketPrice20 + ' euro');
+
+// - sconto 40% over 65
+var coupon40 = ((fullTicketPrice * 40) / 100);
+console.log ('sconto over 65: ' + coupon40 + ' euro');
+
+//- prezzo finale per over 65
+var ticketPrice40 = fullTicketPrice - coupon40;
+console.log ('prezzo over 65: ' + ticketPrice40 + ' euro');
+
+
+if ( etaUtente <= 18) {
+  document.getElementById('price').innerHTML = 'Complimenti! Ti abbiamo applicato uno sconto del 20% : ' + ' Prezzo totale: ' + ticketPrice20 + ' euro';
+
+}
+
+else if  (etaUtente >= 65){
+
+  document.getElementById('price').innerHTML = 'Complimenti! Ti abbiamo applicato uno sconto del 40% : ' + ' Prezzo totale: ' + ticketPrice40 + ' euro';
+
+
+}
+else {
+  document.getElementById('price').innerHTML = 'Salve, il prezzo del biglietto è di: ' + fullTicketPrice + ' euro';
+}
